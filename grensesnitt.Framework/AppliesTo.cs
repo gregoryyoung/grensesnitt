764f731e5a549177ba8ec4383fbdbb26edd4f6fe
Henrik <henrik@haf.se>
2011-08-19 18:00:34 +0200

    added some documentation on the main interface

--- grensesnitt.Framework/AppliesTo.cs
+++ grensesnitt.Framework/AppliesTo.cs
@@ -1,8 +1,17 @@
 using System;
 namespace grensesnitt.Framework
 {
+	/// <summary>
+	/// Interface that specifies what type the test fixture is a fixture for. The type specified here
+	/// denotes what interface implementations of will be searched for, from the available assembly sources.
+	/// </summary>
+	/// <typeparam name="T">The type to search for.</typeparam>
 	public interface AppliesTo<T>
 	{
+		/// <summary>
+		/// Method called by the add-in to set the current test subject instance.
+		/// </summary>
+		/// <param name="subject">subject instance</param>
 		void SetSubjectUnderTest(T subject);
 	}
 }
